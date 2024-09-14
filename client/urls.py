d2959e2b3b16659701dd46c0cabd94dc16110d62
Rohit Parodhi <rohitparodhi@Rohits-MacBook-Air.local>
2024-09-14 15:12:29 +0530

    nimap

--- client/urls.py
+++ client/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('clients/', ClientListView.as_view(), name='client-list'),
     path('clientscreate/', ClientCreateView.as_view(), name='client-create'),
     path('clients/<int:pk>/', ClientDetailView.as_view(), name='client-detail'),
-    path('clients/<int:client_id>/projects/', ProjectCreateView.as_view(), name='project-create'),
+    path('clients/<int:client_id/projects/', ProjectCreateView.as_view(), name='project-create'),
     path('projects/', UserProjectListView.as_view(), name='user-projects')
 ]
